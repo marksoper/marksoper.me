bd038661e455be0bb86b0eb177d52bbf6d660203
Mark Soper <masoper@gmail.com>
2013-01-24 22:17:40 -0500

    adding static Bike

--- staticBike.js
+++ staticBike.js
@@ -4,10 +4,17 @@ var mainBike = function() {
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
-  var margin = 30;
+  var margin = 20;
+  var dim;
 
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
+  var resize = function() {
+    dim = Math.min(window.innerHeight, window.innerWidth) - 2*margin;
+    canvas.width  = dim;
+    canvas.height = dim;
+    canvas.style.top = margin + "px";
+    canvas.style.left = Math.max(margin, (window.innerWidth/2 - margin - canvas.width/2)) + "px";
+  };
+  resize();
 
   var zeroPadToSix = function(s) {
     while (s.length < 6) s = "0" + s;
@@ -18,78 +25,87 @@ var mainBike = function() {
   var bodyColor = genColorKeywords["chocolate"];
   var groundColor = genColorKeywords["darkslategrey"];
 
-  var parts = {
-    "frontWheel": {
-      shape: new GEN.Arc(600, 400, 120, 0, Math.PI * 2),
-      color: wheelColor,
-      lineWidth: 64
-    },
-    "rearWheel": {
-      shape: new GEN.Arc(200, 400, 120, 0, Math.PI * 2),
-      color: wheelColor,
-      lineWidth: 64
-    },
-    "torso": {
-      shape: new GEN.Stroke(260, 200, 500, 150, -Math.PI/6),
-      color: bodyColor,
-      lineWidth: 640
-    },
-    "upperLeg": {
-      shape: new GEN.Stroke(460, 300, 260, 200, Math.PI/12),
-      color: bodyColor,
-      lineWidth: 480
-    },
-    "lowerLeg": {
-      shape: new GEN.Stroke(340, 400, 460, 300, Math.PI/12),
-      color: bodyColor,
-      lineWidth: 230
-    },
-    "foot": {
-      shape: new GEN.Stroke(340, 400, 460, 400, -Math.PI/12),
-      color: bodyColor,
-      lineWidth: 128
-    },
-    "upperArm": {
-      shape: new GEN.Stroke(500, 150, 440, 230, -Math.PI/6),
-      color: bodyColor,
-      lineWidth: 320
-    },
-    "lowerArm": {
-      shape: new GEN.Stroke(440, 230, 540, 220, -Math.PI/12),
-      color: bodyColor,
-      lineWidth: 210
-    },
-    "head_0": {
-      shape: new GEN.Stroke(560, 90, 420, 70, Math.PI/3),
-      color: bodyColor,
-      lineWidth: 210
-    },
-    "head_1": {
-      shape: new GEN.Stroke(580, 70, 420, 70, -Math.PI/3),
-      color: bodyColor,
-      lineWidth: 210
-    },
-    "head_2": {
-      shape: new GEN.Stroke(590, 60, 420, 70, -Math.PI/3),
-      color: bodyColor,
-      lineWidth: 210
-    },
-    "ground": {
-      shape: new GEN.Stroke(0, 680, canvas.width, 710, -Math.PI/9),
-      color: groundColor,
-      lineWidth:  480
-    }
+  var parts = function() {
+    return {
+      "ground": {
+        shape: new GEN.Stroke(0, dim, dim, 0.93*dim, -Math.PI/12),
+        color: groundColor,
+        lineWidth: 0.2*dim
+      },
+      "frontWheel": {
+        shape: new GEN.Arc(0.78*dim, 0.66*dim, 0.17*dim, 0, Math.PI * 2),
+        color: wheelColor,
+        lineWidth: 0.07*dim
+      },
+      "rearWheel": {
+        shape: new GEN.Arc(0.22*dim, 0.69*dim, 0.17*dim, 0, Math.PI * 2),
+        color: wheelColor,
+        lineWidth: 0.07*dim
+      },
+      "torso": {
+        shape: new GEN.Stroke(0.30*dim, 0.42*dim, 0.65*dim, 0.32*dim, -Math.PI/6),
+        color: bodyColor,
+        lineWidth: 0.8*dim
+      },
+      "upperLeg": {
+        shape: new GEN.Stroke(0.30*dim, 0.42*dim, 0.6*dim, 0.55*dim, -Math.PI/12),
+        color: bodyColor,
+        lineWidth: 0.6*dim
+      },
+      "lowerLeg": {
+        shape: new GEN.Stroke(0.6*dim, 0.55*dim, 0.43*dim, 0.66*dim, -Math.PI/100),
+        color: bodyColor,
+        lineWidth: 0.4*dim
+      },
+      "foot": {
+        shape: new GEN.Stroke(0.43*dim, 0.66*dim, 0.57*dim, 0.66*dim, -Math.PI/12),
+        color: bodyColor,
+        lineWidth: 0.2*dim
+      },
+      "upperArm": {
+        shape: new GEN.Stroke(0.65*dim, 0.32*dim, 0.6*dim, 0.47*dim, -Math.PI/6),
+        color: bodyColor,
+        lineWidth: 0.4*dim
+      },
+      "lowerArm": {
+        shape: new GEN.Stroke(0.6*dim, 0.47*dim, 0.75*dim, 0.42*dim, -Math.PI/12),
+        color: bodyColor,
+        lineWidth: 0.25*dim
+      },
+      "head": {
+        shape: new GEN.Stroke(0.74*dim, 0.20*dim, 0.5*dim, 0.22*dim, -Math.PI/12),
+        color: bodyColor,
+        lineWidth: 0.40*dim
+      }
+    };
   };
 
-  var part;
-  for (var partName in parts) {
-    part = parts[partName];
-    hexColorStr = zeroPadToSix(part.color.toString(16));
-    context.strokeStyle = "#" + hexColorStr;
-    context.lineWidth = part.lineWidth;
-    console.log("drawing: " + partName);
-    part.shape.draw(context);
-  }
+  var draw = function() {
+    var part;
+    var partsNow = parts();
+    for (var partName in partsNow) {
+      part = partsNow[partName];
+      hexColorStr = zeroPadToSix(part.color.toString(16));
+      context.strokeStyle = "#" + hexColorStr;
+      context.lineWidth = part.lineWidth;
+      console.log("drawing: " + partName);
+      part.shape.draw(context);
+    }
+  };
+  draw();
+
+  var canvasResizing;
+  window.addEventListener("resize", function() {
+    if (canvasResizing) {
+      console.log("window resize event ignored");
+      return;
+    }
+    console.log("window resize event NOT ignored");
+    canvasResizing = true;
+    resize();
+    draw();
+    canvasResizing = false;
+  });
 
   // for debugging
   window.context = context;
